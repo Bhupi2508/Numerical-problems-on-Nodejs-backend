f066176906384f624f69a9580b9458f15c3730ca
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2021-05-12 00:24:51 +0530

    create single route for all the values

--- Controller/controler.js
+++ Controller/controler.js
@@ -13,47 +13,66 @@
 /*
 required files
 */
-const { body, validationResult } = require('express-validator');
+const { check, body, validationResult } = require('express-validator');
 
 
 // single api
 module.exports.api = (req, res) => {
     console.log("req came in cotroller => ", req.body);
-    body('problem', 'Please provide value').isLength({ min: 1 });
-    body('password', 'password is not valid').isLength({ min: 4 });
-    const errors = validationResult(req);
 
-    switch (req.body.problem) {
-        case 1:
-            first(req, res, errors);
-            break;
-        case 2:
-            second(req, res, errors);
-            break;
-        case 3:
-            third(req, res, errors);
-            break;
-        case 4:
-            forth(req, res, errors);
-            break;
-        default:
-            defaultVal(req, res, errors);
-            break;
+    // when the request has invalid parameters or when the request has missing required parameters
+    if (req.body.problem === undefined || req.body.pattern === undefined) {
+        console.log("no");
+        const errors = validationResult(req);
+        return res.status(400).json({ errors: errors.array() });
+    } else {
+        console.log("yes");
+        // check('problem', 'Please provide numeric value').isNumeric().isLength({ min: 1 }).isArray();
+        // check('pattern', 'Please provide valid pattern').isNumeric().isLength({ min: 1 });
+        const errors = validationResult(req);
+        // if (!errors.isEmpty()) {
+        //     return res.status(422).json({ errors: errors.array() })
+        //   }
+
+
+        switch (req.body.pattern) {
+            case 1:
+                first(req, res, errors);
+                break;
+            case 2:
+                second(req, res, errors);
+                break;
+            case 3:
+                third(req, res, errors);
+                break;
+            case 4:
+                forth(req, res, errors);
+                break;
+            default:
+                defaultVal(req, res, errors);
+                break;
+        }
     }
-
 };
 
 
 // first problem
 function first(req, res, errors) {
     console.log("1");
+    let odd = [];
+    let even = [];
+
     var response = {};
     if (errors) {
+        console.log("errohai");
         response.success = false;
         response.error = errors;
         return res.status(422).send(response);
     } else {
-        console.log("ok");
+        let arrayData = req.body.problem;
+        arrayData.array.forEach(element => {
+            console.log("element ", element);
+        });
     }
 }
 
@@ -101,6 +120,19 @@ function forth(req, res, errors) {
 
 
 // default
-function defaultVal(req, res) {
+function defaultVal(req, res, errors) {
+    console.log("default");
+    // create a object which will attached all the values
+    var response = {};
+    if (errors) {
+        response.success = false;
+        response.error = errors;
+        return res.status(422).send(response);
+    }
+
+
+    response.success = false;
+    response.withMessage = 'Please provide one valid problem statements (Eg => 1,2,3 or 4)'
+    return res.status(422).send(response);
 
 }
