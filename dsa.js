fa02e8925028ac52d84e58bdf49a2c38d17c89cc
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-07-20 22:28:34 +0530

    232-implementQueueUsingStack.js file updated

--- dsa.js
+++ dsa.js
@@ -1,9 +1,37 @@
 /**
- * @param {number} n
- * @return {boolean}
+ * @constructor
  */
-var isPowerOfTwo = function(n) {
-    if (n <= 0) return false;
-    if((n & (n-1)) === 0) return true;
+var Queue = function() {
+    this.stack = [];
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Queue.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @returns {void}
+ */
+Queue.prototype.pop = function() {
+    this.stack.shift();
+};
+
+/**
+ * @returns {number}
+ */
+Queue.prototype.peek = function() {
+    return this.stack[0];
+};
+
+/**
+ * @returns {boolean}
+ */
+Queue.prototype.empty = function() {
+    var length = this.stack.length;
+    if (length === 0) return true;
     return false;
 };
