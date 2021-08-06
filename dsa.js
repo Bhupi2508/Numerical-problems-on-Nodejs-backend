0aa9b38a3e23c3e3ebe01aef96226030a0c7af14
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-08-06 22:28:46 +0530

    70-climbChairs.js file updated

--- dsa.js
+++ dsa.js
@@ -1,19 +1,52 @@
 /**
- * @param {number} x
+ * @param {number} n
  * @return {number}
  */
-var reverse = function(x) {
-    var revX = 0;
-    var absX = Math.abs(x);
-    while (absX > 0) {
-        var last = absX % 10;
-        revX = revX * 10 + last;
-        absX = (absX - last) / 10;
+// interative
+var climbStairs = function(n) {
+    var result = 0;
+    var stepOne = 1;
+    var stepTwo = 2;
+    if (n === 1) return stepOne;
+    if (n === 2) return stepTwo;
+
+    for (var i = 3; i <= n; i++) {
+        result = stepOne + stepTwo;
+        stepOne = stepTwo;
+        stepTwo = result;
     }
 
-    // in case overflow, in JavaScript maximum number is 2^31 - 1
-    if (revX > Math.pow(2, 31) - 1) return 0;
+    return result;
+};
 
-    if (x < 0) revX = -revX;
-    return revX;
+// 2nd try
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+    var stepOne = 1;
+    var stepTwo = 2;
+    var result;
+    for (var i = 2; i < n; i++) {
+        result = stepOne + stepTwo;
+        stepOne = stepTwo;
+        stepTwo = result;
+    }
+
+    return result;
+};
+
+// recursive, exponential complexity. Not accepted
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    if (n > 2) return climbStairs(n-2) + climbStairs(n-1);
+};
+
+// not optimal
+var climbStairs = function(n) {
+    var f = [1, 2];
+    for (var i = 2; i < n; i++) {
+        f[i] = f[i-1] + f[i-2];
+    }
+
+    return f[n - 1];
 };
