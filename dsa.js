5dce033eff48f71f7b88919e6bde3e94c50f93d0
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-08-18 22:28:31 +0530

    206-reversedLinkedList.js file updated

--- dsa.js
+++ dsa.js
@@ -1,57 +1,50 @@
 /**
- * A simply solution but not efficient consider every character of ‘str1′ and
- * check if all occurrences of it map to same character occurrences in ‘str2′.
- * Time complexity of this solution is O(n*n).
- *
- * This is a better solution (NOT ACCEPTED, Time limit exceeded).
- * 1. check if t[i] is in the map. If yes, find out the key associated with t[i]
- * if key exists, but s[i] is not equal to key, return false;
- * if key is null, but s[i] exists in the map and map[s[i]] is not equal to t[i]
- * return false.
- *
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var isIsomorphic = function(s, t) {
-  var map = {};
-  for (var i = 0; i < s.length; i++) {
-      var key = map.getKeyByValue(t[i]);
-      if (key && s[i] !== key) {
-          return false;
-      } else if (s[i] in map) {
-          if (map[s[i]] !== t[i]) {
-              return false;
-          }
-      } else {
-          map[s[i]] = t[i];
-      }
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// Iterative way
+var reverseList = function(head) {
+  if (head === null || head.next === null)  return head;
+  var headNext = head.next;
+  head.next = null;
+  while (head !== null && headNext !== null) {
+    // keep a reference of the headNext's next node
+    var tmp = headNext.next;
+    headNext.next = head;
+    head = headNext;
+    headNext = tmp;
   }
-  return true;
+
+  return head;
 };
 
-Object.prototype.getKeyByValue = function(value) {
-  for (var prop in this) {
-      if (this.hasOwnProperty(prop)) {
-          if (this[prop] === value)
-              return prop;
-      }
-  }
-};
-
-// a better solution, use array
-var isIsomorphic = function(s, t) {
-    var arrS = [],
-        arrT = [];
-
-    for (var i = 0; i < s.length; i++) {
-        if (arrS[s.charCodeAt(i)] !== arrT[t.charCodeAt(i)]) {
-            return false;
-        }
-
-        arrS[s.charCodeAt(i)] = i;
-        arrT[t.charCodeAt(i)] = i;
+// a more concise solution, prevHead eventually wll be the reversed list head
+var reverseList = function(head) {
+    var prevHead = null;
+    while (head) {
+        var headNext = head.next;
+        head.next = prevHead;
+        prevHead = head;
+        head = headNext;
     }
+    return prevHead;
+};
 
-    return true;
+// recursive way
+var reverseList = function(head) {
+  if (head === null || head.next === null)  return head;
+  var headNext = head.next;
+  head.next = null;
+  var nextReversedListHead = reverseList(headNext);
+  // now headNext is the last node of the reversed List on the right of head!!!
+  headNext.next = head;
+
+  return nextReversedListHead;
 };
