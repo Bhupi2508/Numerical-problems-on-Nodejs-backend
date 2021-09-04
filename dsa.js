645df9a16e83109387074a7928ac79c30f042426
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-09-04 22:28:21 +0530

    104-maxDepth.js file updated

--- dsa.js
+++ dsa.js
@@ -7,107 +7,35 @@
  */
 /**
  * @param {TreeNode} root
- * @return {boolean}
+ * @return {number}
  */
-// recursive
-var isSymmetric = function(root) {
-    if (!root) return true;
-    return isSymmetricHelper(root.left, root.right);
-};
-
-var isSymmetricHelper = function(left, right) {
-  if (!left || !right) return left === right;
-  if (left.val !== right.val) return false;
-  return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
-};
-
-// second try, not good.
-var helper = function(left, right) {
-    if (!left || !right) {
-        if (!left && !right) return true;
-        return false;
-    }
-    if ((left.val === right.val) && helper(left.left, right.right) && helper(left.right, right.left)) {
-        return true;
-    }
-    return false;
+var maxDepth = function(root) {
+  var max = 0;
+  var lmax = maxDepth(root.left);
+  var rmax = maxDepth(root.right);
+  if (lmax > rmax) {
+      root = root.left;
+      max = lmax;
+  } else {
+      root = root.right;
+      max = rmax;
+  }
+  return max + 1;
 }
 
-// third try
-var helper = function(left, right) {
-    if (!left && !right) {
-        return true;
-    } else if (!left || !right) {
-        return false;
-    } else {
-        if (left.val === right.val) {
-            return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
-        } else {
-            return false;
-        }
-    }
-}
-
-// iterative
-var isSymmetric = function(root) {
-    if (!root) return true;
-    var treeStack = [];
-    if (root.left) {
-        if (!root.right) return false;
-        treeStack.push(root.left);
-        treeStack.push(root.right);
-    } else if (root.right) {
-      return false;
-    }
-
-    while(treeStack.length > 0) {
-        if (treeStack.length % 2 !== 0) return false;
-        var right = treeStack.pop();
-        var left = treeStack.pop();
-        if (right.val !== left.val) return false;
-        if (left.left) {
-            if (!right.right) return false;
-            treeStack.push(left.left, right.right);
-        } else if (right.right) return false;
-
-        if (right.left) {
-            if (!left.right) return false;
-            treeStack.push(right.left, left.right);
-        } else if (left.right) return false;
-    }
-
-    return true;
+// second try
+var maxDepth = function(root) {
+    if (!root) return 0;
+    var lHeight = maxDepth(root.left) + 1;
+    var rHeight = maxDepth(root.right) + 1;
+    return lHeight > rHeight ? lHeight : rHeight;
 };
 
-// a second try
-var isSymmetric = function(root) {
-    if (!root) return true;
-    var stack = [];
-    if (root.left && root.right) {
-        stack.push(root.left);
-        stack.push(root.right);
-    } else if (!root.left && !root.right) {
-        return true;
-    } else {
-        return false;
+// simplist
+var maxDepth = function(root) {
+    if (!root) {
+        return 0;
     }
 
-    while (stack.length > 0) {
-        if (stack.length % 2 !== 0) return false;
-        var left = stack.pop();
-        var right = stack.pop();
-        if (left.val !== right.val) return false;
-        if (left.left && right.right) {
-            stack.push(left.left, right.right);
-        } else if ((!left.left && right.right) || (left.left && !right.right)) {
-            return false;
-        }
-        if (right.left && left.right) {
-            stack.push(right.left, left.right);
-        } else if ((!right.left && left.right) || (right.left && !left.right)) {
-            return false;
-        }
-    }
-
-    return true;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
