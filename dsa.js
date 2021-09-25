faef1b8f9fcf448ef88a504f34be23d5e17f922b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-09-25 22:28:25 +0530

    160-IntersectionofTwoLinkedLists.js file updated

--- dsa.js
+++ dsa.js
@@ -1,33 +1,110 @@
 /**
- * @param {string[]} strs
- * @return {string}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return '';
-    var firstString = strs[0];
-    var commonLength = firstString.length;
-    for (var i = 1; i < strs.length; i++) {
-        for (var j = 0; j < commonLength; j++) {
-            if (!strs[i][j]) break;
-            if (firstString[j] !== strs[i][j]) break;
-        }
-        commonLength = j;
+
+/**
+ * because there is an intersection btw two linkedlists, first align the head of longer list with
+ * the shorter one's head. Then just keep moving the heads of both linkedlists
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    var lenA = getLength(headA);
+    var lenB = getLength(headB);
+
+    while(lenA < lenB) {
+        headB = headB.next;
+        lenB--;
     }
 
-    return firstString.substring(0, commonLength);
+    while(lenB < lenA) {
+        headA = headA.next;
+        lenA--;
+    }
+
+    while(headA !== headB) {
+        headA = headA.next;
+        headB = headB.next;
+    }
+
+    return headA;
+
 };
 
-// a better way
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) {
-        return '';
+var getLength = function(listHead) {
+    var length = 0;
+    while (listHead) {
+        length++;
+        listHead = listHead.next;
     }
-    
-    return strs.reduce(function(pre, str) {
-        while (str.indexOf(pre) !== 0) {
-            pre = pre.slice(0, pre.length - 1); 
+    return length;
+};
+
+// 2nd try without knowing the length
+var getIntersectionNode = function(headA, headB) {
+    var intersection = null;
+    var pa = headA;
+    var pb = headB;
+
+    if (!pa || !pb) {
+        return intersection;
+    }
+
+    while (pa || pb) {
+        if (pa && pb && pa.val === pb.val) {
+            intersection = pa;
         }
-        
-        return pre;
-    }, strs[0]);
+
+        // this can be replace by pa === pb
+        while (pa && pb && pa.val === pb.val) {
+            pa = pa.next;
+            pb = pb.next;
+        }
+
+        if (pa === null && pb === null) {
+            break;
+        } else if (pa === null) {
+            pa = headB;
+        } else if (pb === null) {
+            pb = headA;
+        } else {
+            pa = pa.next;
+            pb = pb.next;
+        }
+    }
+
+    return intersection;
+};
+
+// more concise version,  compared to version 2
+var getIntersectionNode = function(headA, headB) {
+    var pa = headA;
+    var pb = headB;
+
+    if (!pa || !pb) {
+        return null;
+    }
+
+    while (pa && pb && pa !== pb) {
+        pa = pa.next;
+        pb = pb.next;
+        if (pa === pb) {
+           return pa;
+        }
+
+        if (!pa) {
+            pa = headB;
+        }
+
+        if (!pb) {
+            pb = headA;
+        }
+    }
+
+    return pa;
 };
