7e02890a25a93a3b636b61c3de4c90ee6dcf6c68
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-11-07 22:28:43 +0530

    367-perfectSquare.js file updated

--- dsa.js
+++ dsa.js
@@ -1,39 +1,28 @@
 /**
- * use one row loop and one col loop to examine all three rules
- * row, column and sub-9-grid (the tricky part).
- *
- * @param {character[][]} board
+ * @param {number} num
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    if (!board || board.length !== 9 || board[0].length !== 9) return false;
+var isPerfectSquare = function(num) {
+    var lo = 1;
+    var hi = num;
+    var isPS = false;
 
-    for (var i = 0; i < board.length; i++) {
-        var row = {};
-        var col = {};
-        var grid = {};
-        for (var j = 0; j < board.length; j++) {
-            if (board[i][j] !== '.' && !(board[i][j] in row)) {
-                row[board[i][j]] = true;
-            } else if (board[i][j] !== '.' && (board[i][j] in row)) {
-                return false;
-            }
+    if (num === 1) {
+        isPS = true;
+    }
 
-            if (board[j][i] !== '.' && !(board[j][i] in col)) {
-                col[board[j][i]] = true;
-            } else if (board[j][i] !== '.' && (board[j][i] in col)) {
-                return false;
-            }
-
-            var rowIndex = 3 * Math.floor(i / 3) + Math.floor(j / 3);
-            var colIndex = 3 * (i % 3) + (j % 3);
-            if (board[rowIndex][colIndex] !== '.' && !(board[rowIndex][colIndex] in grid)) {
-                grid[board[rowIndex][colIndex]] = true;
-            } else if (board[rowIndex][colIndex] !== '.' && (board[rowIndex][colIndex] in grid)) {
-                return false;
-            }
+    while (lo <= hi) {
+        var mid = lo + Math.floor((hi - lo) / 2);
+        var midSquare = mid * mid;
+        if (midSquare === num) {
+            isPS = true;
+            break;
+        } else if (midSquare > num) {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
         }
     }
 
-    return true;
+    return isPS;
 };
