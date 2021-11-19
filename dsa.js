702fbd53848e494eab6ff233e00bb98620c9ea25
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-11-19 22:28:40 +0530

    303-rangeSumQuery.js file updated

--- dsa.js
+++ dsa.js
@@ -1,31 +1,32 @@
 /**
- * Key two rounds
- * 1. first round scan to build the map (number -> appearances), if secret[i] === guess[i], bull++
- * if it's not a bull, increase the cow appearances in the map
- * 2. second round, skip the bull positions and compare the guess number and the map
- * if there is a cow, cow++ and decrease the cow number from the map. if the cow number is 0
- * that is, there is matched cow number between secrect and guess.
+ * Key: Dynamic Programming
+ * add an array to track the sum of previous elements
+ * sums[i] = sum[i-1] + nums[i-1];
+ * sumRange[i, j] = sums[j+1] - sums[i];
  *
- * @param {string} secret
- * @param {string} guess
- * @return {string}
+ * @constructor
+ * @param {number[]} nums
  */
-var getHint = function(secret, guess) {
-    var secretMap = {};
-    var cow = 0;
-    var bull = 0;
-
-    for (var i = 0; i < secret.length; i++) {
-        if (secret[i] === guess[i]) bull++;
-        else if (secretMap[secret[i]]) secretMap[secret[i]]++;
-        else secretMap[secret[i]] = 1;
+var NumArray = function(nums) {
+    this.sums = [0];
+    for (var i = 1; i <= nums.length; i++) {
+        this.sums[i] = this.sums[i - 1] + nums[i - 1];
     }
-    for (var j = 0; j < guess.length; j++) {
-        if (guess[j] !== secret[j] && secretMap[guess[j]]) {
-            cow++;
-            secretMap[guess[j]]--;
-        }
-    }
-
-    return bull + 'A' + cow + 'B';
 };
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    return this.sums[j + 1] - this.sums[i];
+};
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var numArray = new NumArray(nums);
+ * numArray.sumRange(0, 1);
+ * numArray.sumRange(0, 2);
+ */
