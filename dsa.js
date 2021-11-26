e8927d18aaaccafab400dc337bd2d61659dce4a9
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-11-26 22:28:26 +0530

    167-twoSumII.js file updated

--- dsa.js
+++ dsa.js
@@ -1,27 +1,23 @@
 /**
- * @param {string} version1
- * @param {string} version2
- * @return {number}
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
  */
-var compareVersion = function(version1, version2) {
-    var versionOne = version1.split('.');
-    var versionTwo = version2.split('.');
-    if (versionOne.length <= versionTwo.length) {
-        for (var i = versionOne.length; i < versionTwo.length; i++) {
-            versionOne[i] = '0';
-         }
-    }
-    if (versionTwo.length < versionOne.length) {
-        for (var i = versionTwo.length; i < versionOne.length; i++) {
-            versionTwo[i] = '0';
-         }
+var twoSum = function(numbers, target) {
+    var left = 0;
+    var right = numbers.length - 1;
+    var results = [];
+
+    while (left < right) {
+        if (numbers[left] + numbers[right] === target) {
+            results.push(left + 1, right + 1);
+            break;
+        } else if (numbers[left] + numbers[right] < target) {
+            left += 1;
+        } else {
+            right -= 1;
+        }
     }
 
-    for (var i = 0; i < versionOne.length; i++) {
-        var versionOneInt = parseInt(versionOne[i]);
-        var versionTwoInt = parseInt(versionTwo[i])
-        if (versionOneInt < versionTwoInt) return -1;
-        if (versionOneInt > versionTwoInt) return 1;
-    }
-    return 0;
+    return results;
 };
