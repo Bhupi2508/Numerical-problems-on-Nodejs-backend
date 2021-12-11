13927dbdc28f7c8f33d08a12505fe34b21236c2b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-12-11 22:28:39 +0530

    290-wordPattern.js file updated

--- dsa.js
+++ dsa.js
@@ -1,32 +1,53 @@
 /**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * Key: first convert str to an array
+ * Two maps: 1) first map maps pattern to the string array,
+ * 2) second map maps the string to pattern
+ * If just use one map to map the pattern and string, different pattern[i]
+ * can have same string from the string array.
+ *
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
  */
-var moveZeroes = function(nums) {
-  var i = 0, j = nums.length;
-  while (i < j) {
-    if (nums[i] === 0) {
-      nums.splice(i, 1);
-      nums.push(0);
-      j--;
-    } else {
-      i++;
-    }
-  }
+var wordPattern = function(pattern, str) {
+   var strArray = str.split(" ");
+   if (pattern.length !== strArray.length) return false;
+   var patternToStr = {};
+   var strToPattern = {}
+   for (var i = 0; i < pattern.length; i++) {
+       if (pattern[i] in patternToStr) {
+           if (patternToStr[pattern[i]] !== strArray[i]) {
+               return false;
+           }
+       } else if (strArray[i] in strToPattern) {
+           if (strToPattern[strArray[i]] !== pattern[i]) {
+               return false;
+           }
+       }else {
+           patternToStr[pattern[i]] = strArray[i];
+           strToPattern[strArray[i]] = pattern[i];
+       }
+   }
+
+   return true;
 };
 
-// two pointers
-var moveZeroes = function(nums) {
-    var newStart = 0;
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            nums[newStart] = nums[i];
-            newStart++;
+// same as first solution, just a bit concise, 80ms
+var wordPattern = function(pattern, str) {
+    var strArray = str.split(" ");
+    if (pattern.length !== strArray.length) return false;
+    var patternToStr = {};
+    var strToPattern = {}
+    for (var i = 0; i < pattern.length; i++) {
+        if (patternToStr[pattern[i]] && (patternToStr[pattern[i]] !== strArray[i])) {
+            return false;
+        } else if (strToPattern[strArray[i]] && (strToPattern[strArray[i]] !== pattern[i])) {
+            return false;
+        } else {
+            patternToStr[pattern[i]] = strArray[i];
+            strToPattern[strArray[i]] = pattern[i];
         }
     }
 
-    while (newStart < nums.length) {
-        nums[newStart] = 0;
-        newStart++;
-    }
+    return true;
 };
