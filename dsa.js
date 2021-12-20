f5b309f23f3fb9cca39cd4328bbad3d341902418
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2021-12-20 22:28:26 +0530

    169-majorityElement.js file updated

--- dsa.js
+++ dsa.js
@@ -1,23 +1,30 @@
 /**
- * @param {number[]} numbers
- * @param {number} target
- * @return {number[]}
+ * @param {number[]} nums
+ * @return {number}
  */
-var twoSum = function(numbers, target) {
-    var left = 0;
-    var right = numbers.length - 1;
-    var results = [];
-
-    while (left < right) {
-        if (numbers[left] + numbers[right] === target) {
-            results.push(left + 1, right + 1);
-            break;
-        } else if (numbers[left] + numbers[right] < target) {
-            left += 1;
-        } else {
-            right -= 1;
-        }
+// accepted, but not good.
+var majorityElement = function(nums) {
+    var appears = {};
+    for (var i = 0; i < nums.length; i++) {
+       if(appears[nums[i]]) {
+           appears[nums[i]]++;
+       } else {
+           appears[nums[i]] = 1;
+       }
     }
 
-    return results;
+    for (key in appears) {
+        if (appears[key] >= nums.length / 2) {
+            return parseInt(key);
+        }
+    }
+};
+
+// better solution
+var majorityElement = function(nums) {
+    nums.sort(function(a, b) {
+        return a - b;
+    });
+    var mid = Math.floor(nums.length / 2);
+    return nums[mid];
 };
