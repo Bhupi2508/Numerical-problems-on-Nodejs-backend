4f315a8859be841503d8341d7fa07973651df9ad
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-01-21 22:28:24 +0530

    13-romanToInteger.js file updated

--- dsa.js
+++ dsa.js
@@ -1,17 +1,29 @@
 /**
- * Key: use regular expression to find only alpha and numeric letters.
- * Two pointers: start and end
- *
  * @param {string} s
- * @return {boolean}
+ * @return {number}
  */
-var isPalindrome = function(s) {
-    var pattern = /[a-z0-9]/gi;
-    s = s.match(pattern);
-    if (!s || s.length === 1) return true;
-    for (var i = 0; i < s.length / 2; i++) {
-        if (s[i].toLowerCase() !== s[s.length - 1 - i].toLowerCase()) return false;
+var romanToInt = function(s) {
+    var number = 0;
+    var map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    var length = s.length;
+
+    number = map[s[length - 1]];
+    for (var i = length - 2; i >= 0; i--) {
+        if (map[s[i]] < map[s[i + 1]]) {
+            number -= map[s[i]];
+        } else {
+            number += map[s[i]];
+        }
     }
 
-    return true;
+    return number;
+
 };
