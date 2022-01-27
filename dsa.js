7830db1146221a2e19e2833761f6c566fc8751f9
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-01-27 22:28:36 +0530

    242-anagram.js file updated

--- dsa.js
+++ dsa.js
@@ -1,64 +1,22 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function(head) {
-    if (!head || !head.next) return head;
-    var p = head;
-    var pNext = head.next;
-    var newHead = pNext;
-    while (pNext) {
-        p.next = pNext.next;
-        pNext.next = p;
-        var nextPairHead = p;
-        p = p.next;
-        if (p) {
-            pNext = p.next;
-            if (pNext) nextPairHead.next = pNext;
-            else nextPairHead.next = p;
-        } else {
-            pNext = null;
-        }
+// remember: anagram also requires the appearance of the letter in both strings is same.
+var isAnagram = function(s, t) {
+    var letterCount = {};
+    for (var i = 0; i < s.length; i++) {
+       if (letterCount[s[i]]) letterCount[s[i]]++;
+       else letterCount[s[i]] = 1;
+    }
+    for (var i = 0; i < t.length; i++) {
+       if (letterCount[t[i]]) letterCount[t[i]]--;
+       else letterCount[t[i]] = 1;
+    }
+    for (var key in letterCount) {
+        if (letterCount[key] > 0) return false;
     }
 
-    return newHead;
-};
-
-// re write
-var swapPairs = function(head) {
-    if (!head || !head.next) return head;
-    var p = head;
-    var pPrev = head;
-    var newHead = head.next;
-    while (p) {
-        var pNext = p.next;
-        if (pNext) {
-            pPrev.next = pNext;
-            p.next = pNext.next;
-            pNext.next = p;
-            pPrev = p;
-            p = p.next;
-        } else {
-            break;
-        }
-    }
-
-    return newHead;
-};
-
-// recursion version, clean Code
-var swapPairs = function(head) {
-    if (!head || !head.next) return head;
-    var p = head;
-    var pNext = p.next;
-    p.next = swapPairs(pNext.next);
-    pNext.next = p;
-    return pNext;
+    return true;
 };
