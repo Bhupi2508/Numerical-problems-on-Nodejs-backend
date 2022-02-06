7265512aa248fb0489148d1cac6888faed9421dc
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-02-06 22:28:45 +0530

    67-addBinary.js file updated

--- dsa.js
+++ dsa.js
@@ -1,20 +1,46 @@
 /**
- * @param {number[]} digits
- * @return {number[]}
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
  */
-var plusOne = function(digits) {
-    var length = digits.length;
-    var index = length - 1;
-    while (index >= 0) {
-        if (++digits[index] < 10) break;
-        digits[index] -= 10;
-        if (index === 0) {
-            digits.unshift(1);
-            break;
+var addBinary = function(a, b) {
+    var extra = 0;
+    var result = [];
+    var aLength = a.length;
+    var bLength = b.length;
+    var i = 0;
+
+    // add '0' before the shorted binary string
+    if (aLength <= bLength) {
+        i = bLength - 1;
+        for (var j = 0; j < bLength - aLength; j++) {
+            a = '0' + a;
         }
-        index--;
+    } else {
+        i = aLength - 1;
+        for (var j = 0; j < aLength - bLength; j++) {
+            b = '0' + b;
+        }
     }
 
-    return digits;
+    // extra is 1 if there is a carry
+    while (i >= 0) {
+        if (extra === 0) {
+            result[i] = a[i] ^ b[i];
+            if (a[i] === '1' && b[i] === '1') {
+                extra = 1;
+            }
+        } else if (extra === 1) {
+            if (a[i] === '0' && b[i] === '0') {
+                extra = 0;
+                result[i] = '1';
+            } else {
+                result[i] = a[i] & b[i];
+            }
+        }
+        i--;
+    }
+    if (extra === 1) result.unshift('1');
 
+    return result.join('');
 };
