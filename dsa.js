d9187e6984c2b6782d759589c13cda9b97ba430e
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-02-07 22:28:34 +0530

    231-powerOfTwo.js file updated

--- dsa.js
+++ dsa.js
@@ -1,22 +1,9 @@
 /**
- * @param {number[]} nums
- * @return {string[]}
+ * @param {number} n
+ * @return {boolean}
  */
-var summaryRanges = function(nums) {
-    var ranges = [];
-    for (var i = 0; i < nums.length; i++) {
-        var j = i;
-        while (nums[j+1]) {
-            if ((nums[j+1] - nums[j]) === 1) j++;
-            else break;
-        }
-        if (nums[i] !== nums[j]) {
-            ranges.push(nums[i] + '->' + nums[j]);
-        } else {
-            ranges.push('' + nums[i]);
-        }
-        i = j;
-    }
-
-    return ranges;
+var isPowerOfTwo = function(n) {
+    if (n <= 0) return false;
+    if((n & (n-1)) === 0) return true;
+    return false;
 };
