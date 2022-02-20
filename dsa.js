9bfd7fc60c7c44a764d85a3408386d54744c3543
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-02-20 22:28:37 +0530

    26-removeDuplicatesSortedArray.js file updated

--- dsa.js
+++ dsa.js
@@ -1,20 +1,14 @@
 /**
- * @param {number} num
+ * @param {number[]} nums
  * @return {number}
  */
-var addDigits = function(num) {
-  while (num > 9) {
-    num = getTotal(num);
-  }
-  return num;
-};
-
-var getTotal = function(num) {
-  var total = 0, dig = 0;
-  while (num > 9) {
-    dig = num % 10;
-    total += dig;
-    num = Math.floor(num / 10);
-  }
-  return total + num;
+var removeDuplicates = function(nums) {
+    var start = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[start] !== nums[i]) {
+            start++;
+            nums[start] = nums[i];
+        }
+    }
+    return start + 1;
 };
