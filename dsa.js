78027a81b62a9ab0ae786cf429adef21704ec668
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-31 22:28:35 +0530

    24-swapNodesPairs.js file updated

--- dsa.js
+++ dsa.js
@@ -6,12 +6,59 @@
  * }
  */
 /**
- * @param {ListNode} node
- * @return {void} Do not return anything, modify node in-place instead.
+ * @param {ListNode} head
+ * @return {ListNode}
  */
-var deleteNode = function(node) {
-  if (node.next !== null) {
-    node.val = node.next.val;
-    node.next = node.next.next;
-  }
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    var p = head;
+    var pNext = head.next;
+    var newHead = pNext;
+    while (pNext) {
+        p.next = pNext.next;
+        pNext.next = p;
+        var nextPairHead = p;
+        p = p.next;
+        if (p) {
+            pNext = p.next;
+            if (pNext) nextPairHead.next = pNext;
+            else nextPairHead.next = p;
+        } else {
+            pNext = null;
+        }
+    }
+
+    return newHead;
+};
+
+// re write
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    var p = head;
+    var pPrev = head;
+    var newHead = head.next;
+    while (p) {
+        var pNext = p.next;
+        if (pNext) {
+            pPrev.next = pNext;
+            p.next = pNext.next;
+            pNext.next = p;
+            pPrev = p;
+            p = p.next;
+        } else {
+            break;
+        }
+    }
+
+    return newHead;
+};
+
+// recursion version, clean Code
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    var p = head;
+    var pNext = p.next;
+    p.next = swapPairs(pNext.next);
+    pNext.next = p;
+    return pNext;
 };
