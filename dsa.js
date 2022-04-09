711c546d150197caab145256a03dd3371ad5fc5f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-04-09 22:28:46 +0530

    7-reverseInteger.js file updated

--- dsa.js
+++ dsa.js
@@ -1,46 +1,19 @@
 /**
- * @param {string} a
- * @param {string} b
- * @return {string}
+ * @param {number} x
+ * @return {number}
  */
-var addBinary = function(a, b) {
-    var extra = 0;
-    var result = [];
-    var aLength = a.length;
-    var bLength = b.length;
-    var i = 0;
-
-    // add '0' before the shorted binary string
-    if (aLength <= bLength) {
-        i = bLength - 1;
-        for (var j = 0; j < bLength - aLength; j++) {
-            a = '0' + a;
-        }
-    } else {
-        i = aLength - 1;
-        for (var j = 0; j < aLength - bLength; j++) {
-            b = '0' + b;
-        }
+var reverse = function(x) {
+    var revX = 0;
+    var absX = Math.abs(x);
+    while (absX > 0) {
+        var last = absX % 10;
+        revX = revX * 10 + last;
+        absX = (absX - last) / 10;
     }
 
-    // extra is 1 if there is a carry
-    while (i >= 0) {
-        if (extra === 0) {
-            result[i] = a[i] ^ b[i];
-            if (a[i] === '1' && b[i] === '1') {
-                extra = 1;
-            }
-        } else if (extra === 1) {
-            if (a[i] === '0' && b[i] === '0') {
-                extra = 0;
-                result[i] = '1';
-            } else {
-                result[i] = a[i] & b[i];
-            }
-        }
-        i--;
-    }
-    if (extra === 1) result.unshift('1');
+    // in case overflow, in JavaScript maximum number is 2^31 - 1
+    if (revX > Math.pow(2, 31) - 1) return 0;
 
-    return result.join('');
+    if (x < 0) revX = -revX;
+    return revX;
 };
