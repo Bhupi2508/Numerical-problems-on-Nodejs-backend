46bdb53d275e4776d0968b0f77c84404fcc6ad2f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-05-16 22:28:39 +0530

    28-implementstrStr.js file updated

--- dsa.js
+++ dsa.js
@@ -1,34 +1,24 @@
 /**
- * Definition for isBadVersion()
- *
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
  */
-
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
-var solution = function(isBadVersion) {
-    /**
-     * Key: binary search, be careful to set low and high correctly
-     *
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        var low = 1;
-        var high = n;
-        while (low < high) {
-            // be careful in JavaScript, using Math.floor to get the integer part
-            var mid = Math.floor(low + (high - low) / 2);
-            if (!isBadVersion(mid)) low = mid + 1;
-            // and not `mid - 1` to high
-            else high = mid;
+var strStr = function(haystack, needle) {
+    var hIndex = 0;
+    while ((haystack.length - hIndex) >= needle.length) {
+        var nIndex = 0;
+        if (haystack[hIndex] === needle[nIndex]) {
+            while (nIndex < needle.length) {
+                if (haystack[hIndex + nIndex] !== needle[nIndex]) break;
+                nIndex++;
+            }
         }
-        return low;
-    };
+        if (nIndex === needle.length) break;
+        else hIndex++;
+    }
+
+    if ((haystack.length - hIndex) < needle.length) return -1;
+    return hIndex;
 };
+
+// a better solution? KMP?
