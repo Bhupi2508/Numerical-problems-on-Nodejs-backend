6d1674c3603dca08fc0d4ccbe4ba8217e1c1a3c2
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-05-17 22:28:44 +0530

    38-countandSay.js file updated

--- dsa.js
+++ dsa.js
@@ -1,28 +1,29 @@
 /**
- * @param {number} num
- * @return {boolean}
- */
-var isPerfectSquare = function(num) {
-    var lo = 1;
-    var hi = num;
-    var isPS = false;
-
-    if (num === 1) {
-        isPS = true;
-    }
-
-    while (lo <= hi) {
-        var mid = lo + Math.floor((hi - lo) / 2);
-        var midSquare = mid * mid;
-        if (midSquare === num) {
-            isPS = true;
-            break;
-        } else if (midSquare > num) {
-            hi = mid - 1;
-        } else {
-            lo = mid + 1;
+* Key: find the number of occurrences of the number and place the occurrences before the number
+* Be careful about the last element.
+*
+* @param {number} n
+* @return {string}
+*/
+var countAndSay = function(n) {
+    var input = '1';
+    var result = input;
+    for (var i = 1; i < n; i++) {
+        result = '';
+        var count = 1;
+        for (var j = 0; j < input.length; j++) {
+            if (j === input.length - 1) {
+                result = result + count + input[j];
+                break;
+              }
+              if (input[j] === input[j+1]) {
+                  count++;
+              } else {
+                  result = result + count + input[j];
+                  count = 1;
+              }
         }
+        input = result;
     }
-
-    return isPS;
+    return result;
 };
