fbffdc71753ec58fdbd1fd2b95d6c0a02d6d6941
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-05-21 22:28:33 +0530

    228-summaryRanges.js file updated

--- dsa.js
+++ dsa.js
@@ -1,20 +1,22 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * @param {number[]} nums
+ * @return {string[]}
  */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree = function(root) {
-  if (root === null) { return null; }
-  var node = root.left;
-  root.left = root.right;
-  root.right = node;
-  if (root.left) { invertTree(root.left); }
-  if (root.right) { invertTree(root.right); }
-  return root;
+var summaryRanges = function(nums) {
+    var ranges = [];
+    for (var i = 0; i < nums.length; i++) {
+        var j = i;
+        while (nums[j+1]) {
+            if ((nums[j+1] - nums[j]) === 1) j++;
+            else break;
+        }
+        if (nums[i] !== nums[j]) {
+            ranges.push(nums[i] + '->' + nums[j]);
+        } else {
+            ranges.push('' + nums[i]);
+        }
+        i = j;
+    }
+
+    return ranges;
 };
