1e3daf8ca98bd197d1e3a5b4244732e0cedd9c25
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-16 22:28:38 +0530

    27-removeElement.js file updated

--- dsa.js
+++ dsa.js
@@ -1,20 +1,19 @@
 /**
- * @param {number} num
- * @return {boolean}
+ * Key: same solution as 26?
+ * Two pointers
+ *
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
  */
-var isUgly = function(num) {
-  if (num <= 0)  return false; 
-  while (num > 1) {
-    if (num % 2 === 0) {
-      num /= 2;
-    } else if (num % 3 === 0) {
-      num /= 3;
-    } else if (num % 5 === 0) {
-      num /= 5;
-      continue;
-    } else {
-      return false;
+var removeElement = function(nums, val) {
+    var start = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[start] = nums[i];
+            start++;
+        }
     }
-  }
-  return true;
+
+    return start;
 };
